c4888a0e4806542324645c15ded3835bcfdc7762
Maxim Romanov <romanov.maxim@gmail.com>
2018-10-09 11:46:30 +0200

    a working prototype

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HistoryOfIslam.github.io
-Reader Hack/Prototype
+
+* [0748AH] [al-Ḏahabī. *Taʾrīḫ al-islām*. Ed. Baššār ʿAwād Maʿrūf. 1st Ed. Bayrūt: Dār al-Ġarb al-Islāmī, 1424/2003](https://historyofislam.github.io/?/0748Dhahabi/TarikhIslam/BY2003BCM01-ara1/)
+* [0748AH] [al-Ḏahabī. *Taʾrīḫ al-islām*. Ed. ʿUmar ʿAbd al-Salām Tadmuri. 1st Ed. Bayrūt: Dār al-Kitāb al-ʿArabī, 1410/1990](https://historyofislam.github.io/?/0748Dhahabi/TarikhIslam/BY1990TAD01-ara1/)
+* [1089AH] [Ibn al-ʿImād al-Ḥanbalī. *Šaḏarāt al-ḏahab*. Ed. Maḥmūd al-Arnaʾūṭ. 1st Ed. Dimašq-Bayrūt: Dār Ibn Kaṯīr, 1406/1986](https://historyofislam.github.io/?/1089IbnCimad/Shadharat/DM1986ARN01-ara1/)
---
+++ config.js
@@ -0,0 +1,22 @@
+var config = {
+    "1089IbnCimad/Shadharat/DM1986ARN01-ara1": {
+        "fullreference": "Ibn al-ʿImād al-Ḥanbalī. <i>Šaḏarāt al-ḏahab</i>. Ed. Maḥmūd al-Arnaʾūṭ. 1st Ed. Dimašq-Bayrūt: Dār Ibn Kaṯīr, 1406/1986",
+        "fullrefSearch": "ibn imad hanbali shadharat dhahab",
+        "bookURI": "/1089IbnCimad/Shadharat/DM1986ARN01-ara1/"
+    },
+    "0748Dhahabi/TarikhIslam/BY1990TAD01-ara1": {
+        "fullreference": "al-Ḏahabī. <i>Taʾrīḫ al-islām</i>. Ed. ʿUmar ʿAbd al-Salām Tadmuri. 1st Ed. Bayrūt: Dār al-Kitāb al-ʿarabī, 1410/1990",
+        "fullrefSearch": "dhahabi tarikh islam",
+        "bookURI": "/0748Dhahabi/TarikhIslam/BY1990TAD01-ara1/"
+    },
+    "0748Dhahabi/TarikhIslam/BY2003BCM01-ara1": {
+        "fullreference": "al-Ḏahabī. <i>Taʾrīḫ al-islām</i>. Ed. Baššār ʿAwād Maʿrūf. 1st Ed. Bayrūt: Dār al-Ġarb al-Islāmī, 1424/2003",
+        "fullrefSearch": "dhahabi tarikh islam",
+        "bookURI": "/0748Dhahabi/TarikhIslam/BY2003BCM01-ara1/"
+    },
+    "0748Dhahabi/CibarFiKhabar/KW1960SDM02-ara1": {
+        "fullreference": "al-Ḏahabī. <i>al-ʿIbar fī ḫabar man ġabara/i>. Ed. Ṣalāḥ al-dīn al-Munajjid. 2nd Ed. al-Kuwayt: al-Turāṯ al-ʿarabī, 1984",
+        "fullrefSearch": "dhahabi ibar fi khabar man ghabara",
+        "bookURI": "/0748Dhahabi/CibarFiKhabar/KW1960SDM02-ara1/"
+    }
+}
